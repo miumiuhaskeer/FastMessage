95a1d33342e2077d04fbb5cdabe914d9002632d8
Alexey B <bikchurin2001@mail.ru>
2022-04-12 23:37:48 +0300

    Initial commit

---
+++ src/test/java/com/miumiuhaskeer/fastmessage/FastMessageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miumiuhaskeer.fastmessage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FastMessageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
